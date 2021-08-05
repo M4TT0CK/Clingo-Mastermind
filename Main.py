b855ac54a3539e5f4e7ded8c770e41af9889a8c1
James N. Thomas <m4tt0ck@gmail.com>
2021-08-05 10:24:05 -0500

    Adds configurable board size and generalizes ASP

--- Main.py
+++ Main.py
@@ -13,6 +13,8 @@ main_lab.pack()
 
 init_vals = {}
 
+size_options = list(range(1,11))
+
 """
 Initializes a dictionary of empty strings on first start
 """
@@ -37,13 +39,20 @@ def create_grid(n, m, vals):
             label = tk.Label(master=frame, text=vals[f"{j + 1},{i + 1}"], width=3)
             label.pack()
 
+"""
+Clears the grid when user changes size
+"""
+def clear_grid():
+    for widget in grid_frame.winfo_children():
+        widget.destroy()
+
 entries = []
 
 """
 Creates the user input row
 """
 def create_input_row(n):
-    for i in range(n):
+    for i in range(10):
         frame = tk.Frame(
             master=grid_frame,
             relief=tk.RAISED,
@@ -67,9 +76,26 @@ seperator = ttk.Separator(window, orient='horizontal')
 seperator.pack(fill='x')
 
 controls = tk.Frame()
-run = tk.Button(master=controls, text='Run', command=lambda: run())
 
-run.pack(side=LEFT)
+"""
+Resizes grid 10xN (where N <= 10) based on user input
+"""
+def size_selection(event):
+    clear_grid()
+    y = int(value_inside.get())
+    init_vals = create_initial_vals(10, y, {})
+    create_grid(10, y, init_vals)
+    entries.clear()
+    create_input_row(y)
+
+value_inside = tk.IntVar(window)
+value_inside.set(2)
+
+size = tk.OptionMenu(controls, value_inside, *size_options, command=size_selection)
+run_ASP = tk.Button(master=controls, text='Run', command=lambda: run_ASP())
+
+size.pack(side=LEFT)
+run_ASP.pack(side=LEFT)
 
 controls.pack()
 
@@ -84,7 +110,8 @@ def on_model(m):
     sol_list = [x.split(',') for x in sol_list if x]
     sol_list = [[','.join(x[:2]), '2'.join(x[2:])] for x in sol_list]
     vals = {key: value for (key, value) in sol_list}
-    create_grid(10, 2, vals)
+    create_grid(10, int(value_inside.get()), vals)
+
 """
 Grabs the user inputs (which are the totals to be summed) and creates the sums
 in the form of sum(X,Y,N), which are just concatenated together and later interpolated
@@ -94,31 +121,32 @@ def create_sums():
     sums = ''
     x_index = 1
     for entry in entries:
-        sums += f'sum({x_index},3,{entry.get()}). '
+        sums += f'sum({x_index},{entry.get()}). '
         x_index += 1
     return sums
 
 """
 Runs the clingo program which has the sums interoplated into it.
 """
-def run():
+def run_ASP():
     ctl = clingo.Control()
     sums = create_sums()
     ctl.add("base", [], f"""
     n(0..9).
     x(1..10).
-    y(1..2).
+    y(1..{int(value_inside.get())}).
 
     {sums}
 
     % Here are the rules as outlined by https://www.mathinenglish.com/puzzlestenner.php
-
+    
     % You can only have one number from 1 to n
     {{sol(X,Y,N):n(N)}}=1 :- x(X), y(Y).
     % Numbers can only appear once in a row (though they can be repeated in a column)
     :- sol(X,Y,N), sol(A,Y,N), X != A.
     % Numbers in the columns must add up to the given sums
-    :- sol(X,Y,N), sol(X,Y+1,O), sum(X,3,Q), not N+O==Q.
+    sol_sum(X,N) :- sol(X,Y,O), N = #sum{{R : sol(X,Q,R)}}.
+    :- sol_sum(X,N), sum(X,Z), not N==Z.
     % Numbers in the connecting cells must be different (including diagonally)
     :- sol(X,Y,N), sol(X-1,Y,O), not N!=O.
     :- sol(X,Y,N), sol(X+1,Y,O), not N!=O.
@@ -128,8 +156,7 @@ def run():
     :- sol(X,Y,N), sol(X+1,Y+1,O), not N!=O.
     :- sol(X,Y,N), sol(X-1,Y+1,O), not N!=O.
     :- sol(X,Y,N), sol(X+1,Y+1,O), not N!=O.
-
-    #show sol/3. 
+    #show sol/3.
 
     """)
     ctl.ground([("base", [])])
